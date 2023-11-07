1324869bdb0ae9a4a76003f77cf0740ebb1db27a
mariana <marianass712@gmail.com>
2023-11-06 21:30:38 -0300

    se realizo el ejercicio12

---
+++ js/ejercicio12.js
@@ -0,0 +1,3 @@
+/* Realiza un script que genere un número aleatorio entre 1 y 99*/
+const numeroAleatorio = Math.floor (Math.random() * 99)+1; 
+document.write('El número aleatorio es: '+numeroAleatorio);
